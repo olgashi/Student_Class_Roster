6f2be7447d64ad20e170bc9479cdea4c57d98077
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-10-05 14:39:14 -0500

    adding SecurityStudent class

--- securityStudent.cpp
+++ securityStudent.cpp
@@ -2,3 +2,28 @@
 // Created by Olga Shiryaeva on 10/5/19.
 //
 
+SecurityStudent::SecurityStudent():Student()
+{
+    degreeType = SECURITY;
+}
+
+SecurityStudent::SecurityStudent(string studentID, string firstName, string lastName, string studentEmail, int studentAge, int daysToCompleteCourse[], DegreeType degreeType):Student(studentID, firstName, lastName, studentEmail, studentAge, daysToCompleteCourse)
+{
+    degreeType = SECURITY;
+}
+
+DegreeType SecurityStudent::getDegreeType()
+{
+    return SECURITY;
+}
+
+void SecurityStudent::print()
+{
+    this->Student::print();
+    cout << "SECURITY" <<"\n";
+}
+
+SecurityStudent::~SecurityStudent()
+{
+    Student::~Student();
+}
--- securityStudent.h
+++ securityStudent.h
@@ -1,8 +1,27 @@
 //
 // Created by Olga Shiryaeva on 10/5/19.
 //
+#pragma once
+#include <string>
+#include "degree.h"
+#include "student.h"
+class SecurityStudent : public Student
+{
+public:
+//    constructor
+    SecurityStudent();
+    SecurityStudent(
+            string studentID;
+            string firstName;
+            string lastName;
+            string studentEmail;
+            int studentAge;
+            int daysToCompleteCourse[];
+            Degree degreeType;
+            )
 
-#ifndef CLASS_ROSTER_SECURITYSTUDENT_H
-#define CLASS_ROSTER_SECURITYSTUDENT_H
-
-#endif //CLASS_ROSTER_SECURITYSTUDENT_H
+            Degree getDegreeProgram();
+    void print();
+//destructor
+    ~SecurityStudent();
+};
